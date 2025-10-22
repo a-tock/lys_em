beff34aff5a54353a21bc54f3af0bd2e5acc8152
Hidenori Tsuji <h.tsuji.ni.mar91+git@gmail.com>
2025-10-22 13:08:37 +0900

    Modify fitPrecessionDiffraction

--- lys_em/functions.py
+++ lys_em/functions.py
@@ -3,13 +3,13 @@ import itertools
 import numpy as np
 import jax
 import jax.numpy as jnp
+import scipy.optimize as optimize
 from . import TEM, TEMParameter, FunctionSpace, CrystalPotential, multislice
 
 
 def calcSADiffraction(V, crys, numOfCells, Nx=128, Ny=128, division="Auto", tilt=[0, 0]):
     sp = FunctionSpace.fromCrystal(crys, Nx, Ny, numOfCells, division=division)
     pot = CrystalPotential(sp, crys)
-
     tem = TEM(V, params=TEMParameter(tilt=tilt))
     return diffraction(multislice(pot, tem))
 
@@ -18,31 +18,60 @@ def calcPrecessionDiffraction(V, crys, numOfCells, theta, nphi, Nx=128, Ny=128, 
     sp = FunctionSpace.fromCrystal(crys, Nx, Ny, numOfCells, division=division)
     pot = CrystalPotential(sp, crys)
     tem = TEM(V, params=[TEMParameter(tilt=[theta, phi]) for phi in np.arange(0, 360, 360 / nphi)])
-
     return diffraction(multislice(pot, tem)).sum(axis=0)
 
 
-def fitPrecessionDiffraction(V, crys, numOfCells, theta, nphi, Nx=128, Ny=128, division="Auto"):
-    import time
-    start = time.time()
+def fitPrecessionDiffraction(I_hkl, crys, initials, V, theta, nphi, numOfCells, Nx=128, Ny=128, division="Auto", bounds=None, method="L-BFGS-B", tol=1e-2):
+    """
+    Fit the precession diffraction data with the given crystal structure and parameters.
 
-    sp = FunctionSpace.fromCrystal(crys, Nx, Ny, numOfCells, division=division)
-    cpot = CrystalPotential(sp, crys)
+    Args:
+        I_hkl(dict): A dictionary where keys are indices (h, k, l) and values are the corresponding intensities to be fitted.
+        crys (CrystalStructure) : The crystal structure to be used.
+        paramnames (list of str) : The names of the parameters to be fitted.
+        initials (list of float) : The initial values of the parameters to be fitted.
+        V (float) : The acceleration voltage of the TEM.
+        theta (float) : The precession angle of the TEM.
+        nphi (int) : The number of divisions of the precession angle.
+        numOfCells (int) : The number of cells in the multislice simulation.
+        bounds (array_like of tuple, optional) : The bounds of the parameters to be fitted, if None, it will be set to [-inf, inf].
+        method (str, optional) : The optimization method to be used, default is "L-BFGS-B".
+        tol (float, optional) : The tolerance of the optimization, default is 1e-4.
+        Nx (int, optional) : The number of points in the x direction of the multislice simulation, default is 128.
+        Ny (int, optional) : The number of points in the y direction of the multislice simulation, default is 128.
+        division (str or int, optional) : The division method of the multislice simulation, default is "Auto".
+
+    Returns:
+        all_params (dict) : The fitted parameters.
+        res (OptimizeResult) : The result of the optimization.
+    """
+
+    spc = FunctionSpace.fromCrystal(crys, Nx, Ny, numOfCells, division=division)
+    pot = CrystalPotential(spc, crys)
     tem = TEM(V, params=[TEMParameter(tilt=[theta, phi]) for phi in np.arange(0, 360, 360 / nphi)])
 
-    data = diffraction(multislice(cpot, tem)).sum(axis=0).block_until_ready()
-    print("Prec total: ", time.time() - start)
+    def calc_intensity(param):
+        pot_subs = pot.replace(params=param)
+        return diffraction(multislice(pot_subs, tem)).sum(axis=0)
 
-    def R(unit, pos, Uani):
-        pot = cpot.replace(unit=unit, pos=pos, Uani=Uani)
-        return jnp.sum((data - diffraction(multislice(pot, tem)).sum(axis=0))**2)
+    def residual(values):
+        calc = calc_intensity({name: value for name, value in zip(crys.free_symbols, values)})
+        calc_arr = jnp.array([calc[int(indice[0]), int(indice[1])] for indice in I_hkl.keys()])
 
-    g = jax.grad(R, argnums=(0, 1))
-    gr = g(crys.unit, [at.position for at in crys.atoms] , [at.Uani for at in crys.atoms])
-    print(gr)
-    gr = g(crys.unit*0.999, [at.position for at in crys.atoms] , [at.Uani for at in crys.atoms])
-    print(gr)
-    print("Grad: ", time.time() - start)
+        data = jnp.array(list(I_hkl.values()))
+        scale = jnp.sum(data * calc_arr) / jnp.sum(calc_arr**2)
+        R = jnp.linalg.norm(data - scale * calc_arr, ord=1) / jnp.linalg.norm(data, ord=1)
+
+        if not isinstance(scale, jax.core.Tracer):
+            param = {name: float(f"{val:.4g}") for name, val in zip(crys.free_symbols, values)}
+            print(f"Step R = {R:.4f}, scale = {scale:.4g}, params = {param}")
+        return jnp.array(R)
+
+    init = np.array([initials[str(name)] for name in crys.free_symbols])
+    bnd = np.array([bounds[str(name)] if bounds is not None else (-np.inf, np.inf) for name in crys.free_symbols])
+    res = optimize.minimize(residual, init, bounds=bnd, jac=jax.grad(residual), method=method, tol=tol)
+
+    return {name: val for name, val in zip(crys.free_symbols, res.x)}
 
 
 def calcCBED(V, convergence, crys, numOfCells, ndisk=30, Nx=128, Ny=128, division="Auto", sum=True):
@@ -68,6 +97,7 @@ def calc4DSTEM_Crystal(V, convergence, crys, numOfCells, Nx=256, Ny=256, divisio
     res = abs(np.fft.fft2(multislice(pot, tem), axes=(1, 2)))**2
     res.reshape(scanx, scany, Nx, Ny)
     return res
+
 
 def diffraction(data):
     return jnp.abs(jnp.fft.fft2(data))**2
