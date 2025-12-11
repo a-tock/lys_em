78550c12925464a8bfed5f0366ba9b42965d660f
Hidenori Tsuji <h.tsuji.ni.mar91+git@gmail.com>
2025-12-11 22:19:50 +0900

    Modify replace method of GeneralPotential

--- lys_em/potentials/generalPotential.py
+++ lys_em/potentials/generalPotential.py
@@ -1,14 +1,30 @@
 from .interface import PotentialInterface
+from ..space import FunctionSpace
+import jax.numpy as jnp
 
 
 class GeneralPotential(PotentialInterface):
-    def __init__(self, space, potential):
+    def __init__(self, space, potential, isTransFunc=False):
         super().__init__(space)
         self._sp = space
-        self._pot = potential
+        if isTransFunc:
+            self._pot = None
+            self._transFunc = potential
+        else:
+            self._pot = potential
+            self._transFunc = None
 
-    def replace(self, potential):
-        return GeneralPotential(self._sp, potential)
+    def replace(self, potential, isTransFunc=False):
+        return GeneralPotential(self._sp, potential, isTransFunc=isTransFunc)
 
     def getPhase(self, beam):
-        return self._pot
+        if self._pot is not None:
+            return self._pot
+        else:
+            return self._transFunc
+
+    def getTransmissionFunction(self, beam):
+        if self._transFunc is not None:
+            return self._transFunc
+        else:
+            return super().getTransmissionFunction(beam)
